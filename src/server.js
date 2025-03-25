0d962c3a076d919d3b2bf21359d55698bd924d71
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-24 22:33:00 -0300

    Criando banco de dados JSON

--- src/server.js
+++ src/server.js
@@ -1,57 +1,35 @@
-import http from "node:http";
-import { json } from "./middlewares/json.js";
+import http from 'node:http'
+import { Database } from './database.js'
+import { json } from './middlewares/json.js'
 
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-// HTTP Status Code
-
-const users = [];
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
+  const { method, url } = req
 
   await json(req, res)
 
-  if (method === "GET" && url === "/users") {
-    return res
-      .end(JSON.stringify(users));
+  if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
 
-    users.push({
-      id: users.length + 1,
+    const user = {
+      id: 1,
       name,
       email,
-    });
+    }
 
-    return res.writeHead(201).end();
+    database.insert('users', user)
+
+    return res.writeHead(201).end()
   }
 
-  return res.writeHead(404).end();
-});
+  return res.writeHead(404).end()
+})
 
-server.listen(3333);
+server.listen(3333)
